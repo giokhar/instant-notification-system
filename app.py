44dd5d859ec7be227bff228665c6c8bfbe40aded
Giorgi Kharshiladze <giorgi.kharshila@gmail.com>
2019-02-09 23:28:53 -0500

    sending sms to my phone number

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 import json
+from twilio.rest import Client
 
 keys = json.loads(open('keys.json').read())
+account_sid = keys['account_sid']
+auth_token = keys['auth_token']
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="Hi how are you?",
+                     from_=keys['phone_number'],
+                     to='+17654070369'
+                 )
+
+print(message.sid)
