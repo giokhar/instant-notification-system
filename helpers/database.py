9d1ec37396fb1283ce9f688e3413a076bf87dfaa
Davit Kvarskhava <dkvart17@earlham.edu>
2019-02-23 18:24:06 -0500

    register_student implemented and works well

--- helpers/database.py
+++ helpers/database.py
@@ -9,6 +9,8 @@ connection = pymysql.connect(keys['db_host'],keys['db_user'],keys['db_pass'],key
 
 # ALL CUSTOM METHODS TO PULL DATA FROM DATABASE GOES HERE
 
+#If the tuples in the table contain one element,
+#this method converts the table into a list and returns that list.
 def format_sql_result(lst):
 	result = []
 	for next in lst:
@@ -21,17 +23,29 @@ def get_phone_nums(floor_ids):
 	floor_ids = floor_ids.split(',')
 
 	with connection.cursor() as cursor:
-		format_strings = ','.join(['%s'] * len(floor_ids))
+		format_strings = ','.join(['%s'] * len(floor_ids)) #format the floor_ids so it fils sql query
+		print(format_strings)
 		cursor.execute("SELECT phone FROM students where floor_id IN (%s)" % format_strings, tuple(floor_ids))
 		result = cursor.fetchall()
 
 	result = format_sql_result(result)
 	return result
 
+#Returns a table(tuple of tuples) of all students(fn,ln,email,phone) with their hall and floor names.
 def get_all_students():
 	with connection.cursor() as cursor:
 		cursor.execute("SELECT students.first, students.last, students.email, halls.name, floors.name, students.phone FROM students INNER JOIN floors ON students.floor_id=floors.id INNER JOIN halls ON halls.id=floors.hall_id")
 		result = cursor.fetchall()
-	print(result)
+	return result
 
-get_all_students()
+#Each argument is given as a string.
+#Adds a new student into the database.
+def register_student(first, last, email, floor_id, phone):
+	with connection.cursor() as cursor:
+
+		format_strings = ','.join(['%s'] * 5) #argc == 5
+
+		cursor.execute("INSERT INTO students (first, last, email, floor_id, phone) VALUES (%s)" % format_strings, (first, last, email, floor_id, phone))
+
+		connection.commit()
+
