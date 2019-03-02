90395c929cf427f393f77fed9327663a60d37ce6
Giorgi Kharshiladze <giorgi.kharshila@gmail.com>
2019-03-02 15:23:50 -0500

    unread badges and unread coloring works

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, redirect, render_template
 from flask_socketio import SocketIO
 # custom imports
 from helpers.twilio import process_response, send_mass_message
-from helpers.database import keys, get_all_students, get_alert_names, get_alert_template, get_audience_names, get_last_read_student_id, get_all_chat_messages_with, get_students_recent_messages_with_unread_count
+from helpers.database import keys, get_all_students, get_alert_names, get_alert_template, get_audience_names, get_last_read_student_id, get_all_chat_messages_with, get_students_recent_messages_with_unread_count,edit_unread_count
 from helpers.custom import format_floor_ids
 
 app = Flask(__name__, static_url_path='/static')
@@ -41,14 +41,14 @@ def mass_message_page():
 
 @app.route("/chat")
 def chat_page():
-	# GET THE MOST RECENT USER ID BASED ON LAST MESSAGE
 	student_id = get_last_read_student_id()
 	return redirect('/chat/'+str(student_id))
 
 @app.route("/chat/<student_id>", methods=['GET', 'POST'])
 def chat_user_page(student_id):
-	students = get_students_recent_messages_with_unread_count()
-	messages = get_all_chat_messages_with(student_id)
+	edit_unread_count(student_id, 0) # clear unread messages when I open it
+	students = get_students_recent_messages_with_unread_count() # get sidebar info
+	messages = get_all_chat_messages_with(student_id) # get chat history
 	return render_template('chat.html', messages=messages, students=students)
 
 @app.route("/about")
