ddb057aa33ec708293ad6e643f2b51fab096a965
Giorgi Kharshiladze <giorgi.kharshila@gmail.com>
2019-03-03 15:20:47 -0500

    image sending not working, debug mode

--- helpers/twilio.py
+++ helpers/twilio.py
@@ -50,51 +50,51 @@ def process_response(request, socketio):
 		phone = request.values['From']
 		text = request.values['Body'].strip()
 
-		try:
-			# Check if user phone number exists and then let them add the image
-			student_id = db.get_student_id(phone) #Throws an error if such student doesn't exist.
+		# try:
+		# Check if user phone number exists and then let them add the image
+		student_id = db.get_student_id(phone) #Throws an error if such student doesn't exist.
 
 
-			#if this student has never sent a message before
-			if not db.get_all_chat_messages_with(student_id):
-				send_message(phone, "Hello, You can report or talk to public safety.\nYour report should be in the following format: \n$MY REPORT")
-			#_____________________________________
-			
-			#DEFAULT
-			is_report = False
-			is_sender = True
-			is_img = (request.values['NumMedia'] != '0')
+		#if this student has never sent a message before
+		if not db.get_all_chat_messages_with(student_id):
+			send_message(phone, "Hello, You can report or talk to public safety.\nYour report should be in the following format: \n$MY REPORT")
+		#_____________________________________
+		
+		#DEFAULT
+		is_report = False
+		is_sender = True
+		is_img = (request.values['NumMedia'] != '0')
 
-			if text != "":
-				if text[0] == "$":
-					is_report = True
-					text=text[1:]
-					send_message(phone, "Thank you for your report!")
+		if text != "":
+			if text[0] == "$":
+				is_report = True
+				text=text[1:]
+				send_message(phone, "Thank you for your report!")
 
-			if is_img: #processing the images sent via text message.
-				filename = request.values['MessageSid']+'.png'
-				text = "downloads/"+filename
-				process_image(request, filename, student_id) # download image first
-				socketio.emit('message_received', {"student_id":student_id, "message":text, "name":db.get_student_name(student_id), "is_img":is_img, "is_report":is_report}) # then emit the socket
-				socketio.emit('add_message_to_header', {"student_id":student_id, "message":"Image", "name":db.get_student_name(student_id) }) # add message to the header panel
-			elif is_report: # processing reports
-				socketio.emit('message_received', {"student_id":student_id, "message":text, "name":db.get_student_name(student_id), "is_img":is_img, "is_report":is_report}) # when regular text message is sent
-				socketio.emit('add_report_to_header', {"student_id":student_id, "message":text, "name":db.get_student_name(student_id)})
-				db.insert_to_chat_messages(student_id, text, datetime.now(), is_sender, is_report, is_img)
-				db.edit_unread_count(student_id, opr=1)
-			else:
-				socketio.emit('message_received', {"student_id":student_id, "message":text, "name":db.get_student_name(student_id), "is_img":is_img, "is_report":is_report}) # when regular text message is sent
-				socketio.emit('add_message_to_header', {"student_id":student_id, "message":text, "name":db.get_student_name(student_id)})
-				db.insert_to_chat_messages(student_id, text, datetime.now(), is_sender, is_report, is_img)
-				db.edit_unread_count(student_id, opr=1)
-		except:
-			# In this case, user sent something but this phone number does not exist in the database
-			# ASK USER TO REGISTER
-			if is_valid_email(text):
-				db.edit_student_phone(text.lower(), phone)
-				send_message(phone, "Your phone number was successfully added to our database!")
-			else:
-				send_message(phone, "Your phone number is not in our database. Please reply with your valid email in order to register.")
+		if is_img: #processing the images sent via text message.
+			filename = request.values['MessageSid']+'.png'
+			text = "downloads/"+filename
+			process_image(request, filename, student_id) # download image first
+			socketio.emit('message_received', {"student_id":student_id, "message":text, "name":db.get_student_name(student_id), "is_img":is_img, "is_report":is_report}) # then emit the socket
+			socketio.emit('add_message_to_header', {"student_id":student_id, "message":"Image", "name":db.get_student_name(student_id) }) # add message to the header panel
+		elif is_report: # processing reports
+			socketio.emit('message_received', {"student_id":student_id, "message":text, "name":db.get_student_name(student_id), "is_img":is_img, "is_report":is_report}) # when regular text message is sent
+			socketio.emit('add_report_to_header', {"student_id":student_id, "message":text, "name":db.get_student_name(student_id)})
+			db.insert_to_chat_messages(student_id, text, datetime.now(), is_sender, is_report, is_img)
+			db.edit_unread_count(student_id, opr=1)
+		else:
+			socketio.emit('message_received', {"student_id":student_id, "message":text, "name":db.get_student_name(student_id), "is_img":is_img, "is_report":is_report}) # when regular text message is sent
+			socketio.emit('add_message_to_header', {"student_id":student_id, "message":text, "name":db.get_student_name(student_id)})
+			db.insert_to_chat_messages(student_id, text, datetime.now(), is_sender, is_report, is_img)
+			db.edit_unread_count(student_id, opr=1)
+		# except:
+		# 	# In this case, user sent something but this phone number does not exist in the database
+		# 	# ASK USER TO REGISTER
+		# 	if is_valid_email(text):
+		# 		db.edit_student_phone(text.lower(), phone)
+		# 		send_message(phone, "Your phone number was successfully added to our database!")
+		# 	else:
+		# 		send_message(phone, "Your phone number is not in our database. Please reply with your valid email in order to register.")
 	return 1
 # method to check if given email is valid format
 # and check if such email is in our database
