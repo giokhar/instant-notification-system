1ac75f92de659b191aa2100c2e02339b12169bd4
Giorgi Kharshiladze <giorgi.kharshila@gmail.com>
2019-03-03 14:38:43 -0500

    transports websocket polling bug fix attempt

--- static/assets/js/custom.js
+++ static/assets/js/custom.js
@@ -1,5 +1,5 @@
 var url = 'https://' + document.domain + ':' + location.port;
-var socket = io.connect(url);
+var socket = io.connect(url, { transports: ['websocket', 'polling'] });
 
 function getCurrentTime(){
     return new Date().toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
